ac9fbdf423c0d8196d33409df037c347ee0b5d28
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-04-07 21:42:29 +0700

    dep(): install dependencies

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,3 +1,7 @@
+import { AppConfigModule } from './app/app.config.module';
 import { MongoConfigModule } from './mongo/mongo.config.module';
 
-export const CONFIG_MODULES = [MongoConfigModule];
+export const CONFIG_MODULES = [
+  // MongoConfigModule
+  AppConfigModule,
+];
