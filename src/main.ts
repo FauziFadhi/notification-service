200acac85f651ce0459f701b7641a4d1260fe9e5
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-04-07 14:01:32 +0700

    feat(config): add  app config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { engines } from '../package.json';
+import { satisfies } from 'semver';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const version = engines.node;
+  if (!satisfies(process.version, version)) {
+    console.log(
+      `Required node version ${version} not satisfied with current version ${process.version}.`,
+    );
+    process.exit(1);
+  }
+
+  const configService = app.get(ConfigService);
+  const appPort = configService.get<number>('app.port');
+  await app.listen(appPort || 3000, '0.0.0.0');
 }
 bootstrap();
