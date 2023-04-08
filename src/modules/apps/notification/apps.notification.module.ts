2ec24a64ce25fb8a3b18ff5189e3178ffc4cee60
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-04-08 14:34:57 +0700

    feat(notification): add implementation service to factory type

--- src/modules/apps/notification/apps.notification.module.ts
+++ src/modules/apps/notification/apps.notification.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NotificationChannelModule } from 'src/modules/middleware/notification-channel/notification.channel.module';
+import { HappyBirthdayNotificationService } from './services/happy-birthday.notification.service';
+import { LeaveBalanceNotificationService } from './services/leave-balance.notification.service';
+import { MonthlyPaymentSlipNotificationService } from './services/mothly-payment-slip.notification.service';
 import { NotificationFactory } from './services/notification.service';
 
 @Module({
   imports: [NotificationChannelModule],
-  providers: [NotificationFactory],
+  providers: [
+    NotificationFactory,
+    HappyBirthdayNotificationService,
+    MonthlyPaymentSlipNotificationService,
+    LeaveBalanceNotificationService,
+  ],
   exports: [],
 })
 export class AppsNotificationModule {}
--- src/modules/apps/notification/services/notification.service.ts
+++ src/modules/apps/notification/services/notification.service.ts
@@ -1,14 +1,25 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ENotificationType } from '../notification.enum';
+import { HappyBirthdayNotificationService } from './happy-birthday.notification.service';
 import { INotification } from './interfaces/notification.interface';
+import { LeaveBalanceNotificationService } from './leave-balance.notification.service';
+import { MonthlyPaymentSlipNotificationService } from './mothly-payment-slip.notification.service';
 
 @Injectable()
 export class NotificationFactory {
-  private notificationType: Record<ENotificationType, INotification> = {
-    // [ENotificationType.HappyBirthday]
+  constructor(
+    private readonly happyBirthdayService: HappyBirthdayNotificationService,
+    private readonly monthlyPaymentSlipService: MonthlyPaymentSlipNotificationService,
+    private readonly leaveBalanceService: LeaveBalanceNotificationService,
+  ) {}
+
+  private notificationType: Record<ENotificationType, INotification<any>> = {
+    [ENotificationType.HappyBirthday]: this.happyBirthdayService,
+    [ENotificationType.LeaveBalanceReminder]: this.leaveBalanceService,
+    [ENotificationType.MonthlyPayslip]: this.monthlyPaymentSlipService,
   };
 
-  setType(type: ENotificationType): INotification {
+  setType(type: ENotificationType): INotification<any> {
     const notificationByType = this.notificationType[type];
 
     if (!notificationByType) {
