25f3854f38bc58a7a2234d572ee7ce7eb07a33b1
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-04-08 14:27:57 +0700

    feat(notif): add happy birthday implementation service

--- src/modules/apps/notification/services/interfaces/notification.interface.ts
+++ src/modules/apps/notification/services/interfaces/notification.interface.ts
@@ -1,9 +1,17 @@
+import { ENotificationChannel } from 'src/modules/middleware/notification-channel/interfaces/notification.channel.interface';
 import { ENotificationType } from '../../notification.enum';
 
 export interface INotificationBirthday {
   name: ENotificationType.HappyBirthday;
   params: {
     name: string;
+  };
+}
+
+export interface INotificationLeaveBalance {
+  name: ENotificationType.LeaveBalanceReminder;
+  params: {
+    date: string;
   };
 }
 
@@ -19,6 +27,19 @@ export interface IUserContact {
   email: string;
 }
 
-export interface INotification {
-  send(dto: unknown): Promise<any>;
+export type NotificationDTO<type extends ENotificationType> = {
+  contact: IUserContact;
+} & (type extends ENotificationType.HappyBirthday
+  ? INotificationBirthday
+  : type extends ENotificationType.MonthlyPayslip
+  ? INotificationPayment
+  : type extends ENotificationType.LeaveBalanceReminder
+  ? INotificationLeaveBalance
+  : never);
+
+export interface INotification<type extends ENotificationType> {
+  send(
+    dto: NotificationDTO<type>,
+    subscribedChannels: ENotificationChannel[],
+  ): Promise<any>;
 }
