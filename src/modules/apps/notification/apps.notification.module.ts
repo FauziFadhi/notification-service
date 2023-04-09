bdf15b9136dc3099c0c0bc6c6a0f4d6d3bed6214
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-04-09 14:43:33 +0700

    feat(ui-notif): add service for listing users ui notif

--- src/modules/apps/notification/apps.notification.module.ts
+++ src/modules/apps/notification/apps.notification.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from 'src/microservices/account-service/user/user.module';
 import { CompanyModule } from 'src/microservices/company-service/company.module';
+import {
+  UserNotification,
+  UserNotificationSchema,
+} from 'src/models/mongo/user-notification';
 import { NotificationChannelModule } from 'src/modules/middleware/notification-channel/notification.channel.module';
 import { AppsNotificationController } from './controllers/apps.notification.controller';
+import { AppsUserNotificationController } from './controllers/apps.user-notification.controller';
 import { HappyBirthdayNotificationService } from './services/happy-birthday.notification.service';
 import { LeaveBalanceNotificationService } from './services/leave-balance.notification.service';
 import { MonthlyPaymentSlipNotificationService } from './services/mothly-payment-slip.notification.service';
@@ -10,15 +16,23 @@ import { NotificationFactory } from './services/notification.factory';
 import { NotificationService } from './services/notification.service';
 
 @Module({
-  imports: [NotificationChannelModule, UserModule, CompanyModule],
+  imports: [
+    NotificationChannelModule,
+    UserModule,
+    CompanyModule,
+    MongooseModule.forFeature([
+      { name: UserNotification.name, schema: UserNotificationSchema },
+    ]),
+  ],
   providers: [
     NotificationFactory,
     HappyBirthdayNotificationService,
     MonthlyPaymentSlipNotificationService,
     LeaveBalanceNotificationService,
     NotificationService,
+    Logger,
   ],
-  controllers: [AppsNotificationController],
+  controllers: [AppsNotificationController, AppsUserNotificationController],
   exports: [],
 })
 export class AppsNotificationModule {}
---
+++ src/utils/helpers.ts
@@ -0,0 +1,40 @@
+import { ClassTransformOptions, plainToInstance } from 'class-transformer';
+
+export const circularToJSON = (circular: unknown) =>
+  JSON.parse(JSON.stringify(circular));
+
+/**
+ * `options.raw` = `true` for using pure object that you provided.
+ * `options.raw` = `false` | `undefined` for coverting circular structure to JSON
+ * @param cls
+ * @param obj
+ * @param options
+ */
+export function transformer<T, V extends { new (...args: unknown[]): T }>(
+  cls: V,
+  obj: ConstructorParameters<V>[0],
+  options?: ClassTransformOptions & { raw?: boolean },
+): T;
+export function transformer<T, V extends { new (...args: unknown[]): T }>(
+  cls: V,
+  obj: ConstructorParameters<V>[0][],
+  options?: ClassTransformOptions & { raw?: boolean },
+): T[];
+export function transformer<T, V extends { new (...args: unknown[]): T }>(
+  cls: V,
+  obj: ConstructorParameters<V>[0] | ConstructorParameters<V>[0][],
+  options?: ClassTransformOptions & { raw?: boolean },
+) {
+  const result = plainToInstance(
+    cls,
+    options?.raw ? obj : circularToJSON(obj),
+    {
+      excludeExtraneousValues: true,
+      exposeUnsetFields: true,
+      // enableImplicitConversion: true,
+      // exposeDefaultValues: true,
+      ...options,
+    },
+  );
+  return result as unknown;
+}
